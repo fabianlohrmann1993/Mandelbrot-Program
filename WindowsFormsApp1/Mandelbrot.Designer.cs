fffb3f84e3d23055c09fa4dbedc9c0854129a0b2
Koeleman, J.A.L. (Jitske) <j.a.l.koeleman@students.uu.nl>
2018-10-05 14:30:15 +0200

    Kleurenplaatjes, Klikken gefixed, helemaal afgemaakt. Klaar om in te leveren

--- WindowsFormsApp1/Mandelbrot.Designer.cs
+++ WindowsFormsApp1/Mandelbrot.Designer.cs
@@ -38,13 +38,14 @@
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.panel2 = new System.Windows.Forms.Panel();
+            this.listBox1 = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(496, 66);
+            this.button1.Location = new System.Drawing.Point(373, 66);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(77, 32);
+            this.button1.Size = new System.Drawing.Size(59, 32);
             this.button1.TabIndex = 0;
             this.button1.Text = "OK";
             this.button1.UseVisualStyleBackColor = true;
@@ -54,30 +55,30 @@
             // 
             this.TmidX.Location = new System.Drawing.Point(106, 29);
             this.TmidX.Name = "TmidX";
-            this.TmidX.Size = new System.Drawing.Size(182, 22);
+            this.TmidX.Size = new System.Drawing.Size(113, 22);
             this.TmidX.TabIndex = 1;
             this.TmidX.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // Tscale
             // 
-            this.Tscale.Location = new System.Drawing.Point(391, 29);
+            this.Tscale.Location = new System.Drawing.Point(297, 29);
             this.Tscale.Name = "Tscale";
-            this.Tscale.Size = new System.Drawing.Size(182, 22);
+            this.Tscale.Size = new System.Drawing.Size(135, 22);
             this.Tscale.TabIndex = 2;
             this.Tscale.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
             // Tmaxgetal
             // 
-            this.Tmaxgetal.Location = new System.Drawing.Point(391, 71);
+            this.Tmaxgetal.Location = new System.Drawing.Point(297, 71);
             this.Tmaxgetal.Name = "Tmaxgetal";
-            this.Tmaxgetal.Size = new System.Drawing.Size(99, 22);
+            this.Tmaxgetal.Size = new System.Drawing.Size(52, 22);
             this.Tmaxgetal.TabIndex = 3;
             // 
             // TmidY
             // 
             this.TmidY.Location = new System.Drawing.Point(106, 71);
             this.TmidY.Name = "TmidY";
-            this.TmidY.Size = new System.Drawing.Size(182, 22);
+            this.TmidY.Size = new System.Drawing.Size(113, 22);
             this.TmidY.TabIndex = 4;
             this.TmidY.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
             // 
@@ -102,7 +103,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(330, 32);
+            this.label3.Location = new System.Drawing.Point(236, 32);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(55, 17);
             this.label3.TabIndex = 7;
@@ -112,7 +113,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(348, 74);
+            this.label4.Location = new System.Drawing.Point(254, 74);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(37, 17);
             this.label4.TabIndex = 8;
@@ -127,11 +128,27 @@
             this.panel2.TabIndex = 0;
             this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
             // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Items.AddRange(new object[] {
+            "Standaard",
+            "Kekke kleur",
+            "Nederlandse Vlag",
+            "Regenboog"});
+            this.listBox1.Location = new System.Drawing.Point(450, 29);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 68);
+            this.listBox1.TabIndex = 9;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged_1);
+            // 
             // Mandelbrot
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(582, 647);
+            this.Controls.Add(this.listBox1);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
@@ -161,6 +178,7 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.ListBox listBox1;
     }
 }
 
--- WindowsFormsApp1/Mandelbrot.cs
+++ WindowsFormsApp1/Mandelbrot.cs
@@ -13,33 +13,36 @@ namespace WindowsFormsApp1
 
     public partial class Mandelbrot : Form
     {
- 
         //variable member declared outside of any specific function, in order to make it available throughout the class in all functions
         int mandelBrotGetal;
         int maxgetal = 100;
         double scale = 0.01;
         double midX = 0;
         double midY = 0;
+        string kleurschema;
 
+        // Grootte van de interface & panel
         int width = 500;
         int height = 500;
         int widthForm = 600;
         int heightForm = 700;
   
-
         public Mandelbrot()
         {
+            // De interface wordt gemaakt en de textboxen ingevuld
             InitializeComponent();
             Tmaxgetal.Text = maxgetal.ToString();
             Tscale.Text = scale.ToString();
             TmidX.Text = midX.ToString();
             TmidY.Text = midY.ToString();
-
-
+            listBox1.SelectedIndex = 0;
+            kleurschema = listBox1.Items[0].ToString();
             this.Text = "Mandelbrot";
             this.Size = new Size(widthForm, heightForm);
+
+            // Panel tekening 
             panel2.Paint += this.tekenscherm;
-            this.Click += this.button1_Click;
+            this.button1.Click += this.button1_Click;
             panel2.MouseClick += this.mouse_click;
         }
 
@@ -49,7 +52,6 @@ namespace WindowsFormsApp1
             Bitmap image = new Bitmap(width,height);
 
             //the next few lines deal with applying the mandelFunction to one pixel(x,y)
-
             //we  make a nested for loop here to let it run through every pixel
             for (int y = 0; y < height; y++)
             {
@@ -80,34 +82,117 @@ namespace WindowsFormsApp1
                         afstand = System.Math.Sqrt(varPunt.A * varPunt.A + varPunt.B * varPunt.B);
                     }
 
-                    // convert the mandelbrotGetal to a color
-                    // even mandelbrotGetallen become white, uneven become black, infinite also become black
+                    // Convert the mandelbrotGetal to a color
+                    // De verschillende kleurcombinaties van de standaardplaatjes worden hier gecreëerd. 
+
                     Color kleur;
-                    if (mandelBrotGetal == maxgetal)
+
+                    if (kleurschema == "Standaard")
                     {
-                        kleur = Color.Black;
+                        if (mandelBrotGetal == maxgetal)
+                        {
+                            kleur = Color.Black;
+                        }
+                        else if (mandelBrotGetal % 2 == 0)
+                        {
+                            kleur = Color.White;
+                        }
+                        else
+                        {
+                            kleur = Color.Black;
+                        }
                     }
-                    else if (mandelBrotGetal % 2 == 0)
+
+                    else if (kleurschema == "Kekke kleur")
                     {
-                        kleur = Color.White;
+                        if (mandelBrotGetal == maxgetal)
+                        {
+                            kleur = Color.Honeydew;
+                        }
+                        else if (mandelBrotGetal % 2 == 0)
+                        {
+                            kleur = Color.White;
+                        }
+                        else if (mandelBrotGetal % 3 == 0)
+                        {
+                            kleur = Color.Coral;
+                        }
+                        else
+                        {
+                            kleur = Color.Honeydew;
+                        }
                     }
+
+                    else if (kleurschema == "Nederlandse Vlag")
+                    {
+                        if (mandelBrotGetal == maxgetal)
+                        {
+                            kleur = Color.Orange;
+                        }
+                        else if (mandelBrotGetal % 7 == 0)
+                        {
+                            kleur = Color.DarkOrange;
+                        }
+
+                        else if (mandelBrotGetal % 2 == 0)
+                        {
+                            kleur = Color.White;
+                        }
+                        else if (mandelBrotGetal % 3 == 0)
+                        {
+                            kleur = Color.Red;
+                        }
+                        else
+                        {
+                            kleur = Color.Blue;
+                        }
+                    }
+
+                    else if (kleurschema == "Regenboog")
+                    {              
+                        if (mandelBrotGetal == maxgetal)
+                        {
+                            kleur = Color.Purple;
+                        }
+                        else if (mandelBrotGetal % 11 == 0)
+                        {
+                            kleur = Color.Blue;
+                        }                       
+                        else if (mandelBrotGetal % 4 == 0)
+                        {
+                            kleur = Color.Green;
+                        }
+                        else if (mandelBrotGetal % 7 == 0)
+                        {
+                            kleur = Color.Yellow;
+                        }
+                        else if (mandelBrotGetal % 3 == 0)
+                        {
+                            kleur = Color.Orange;
+                        }
+                        else
+                        {
+                            kleur = Color.Red;
+                        }
+                    }
+
+                    // Voor als er geen standaardplaatje geselecteerd is
                     else
                     {
                         kleur = Color.Black;
-                    }
-
-                    // TODO: now we have the mandelbrotgetal for any coordinaat(x,y) or pixel(x,y) on the window !
-                    // TODO: here we should paint the given pixel black or white
+                    }                    
+                  
+                    // now we have the mandelbrotgetal for any coordinaat(x,y) or pixel(x,y) on the window !
+                    // Here we should paint the given pixel with the color determined above
                     image.SetPixel(x, y, kleur);
-
-
-                }
-                
+                }               
             }
+
             // Draw image (should be outside for-loops)
             pea.Graphics.DrawImage(image, 0, 0, width, height);
         }
 
+        // Hier wordt het mandelbrotgetal berekend dmv de functie die ons was gegeven
         Punt mandelbrotfunctie(Punt varPunt, Punt coordinaat)
         {
             Punt resultaat = new Punt(0,0);
@@ -119,25 +204,56 @@ namespace WindowsFormsApp1
             return resultaat;
         }
 
+        // Als er op OK wordt geklikt worden de parameters bijgesteld
         private void button1_Click(object sender, EventArgs e)
-        {  
+        {
+            // Poging tot parsen user input
+            try
+            {
                 // User Interface veranderingen aangegeven in de textboxen
                 scale = Double.Parse(Tscale.Text);
                 maxgetal = int.Parse(Tmaxgetal.Text);
                 midX = Double.Parse(TmidX.Text); // moeten we scalen
                 midY = Double.Parse(TmidY.Text);
                 panel2.Invalidate();
+            }
+            
+            // Als de user input invalid is, wordt een foutmelding gegeven. 
+            catch (Exception notnumber)
+            {            
+                Form popup;
+                popup = new Form();
+                popup.Size = new Size (500,200);
+                popup.Text = "Error";
+
+                Label error = new Label();
+                error.Text = "Invalid input. Please only use numbers and the character \",\". ";
+                error.Location = new Point(20, 20);
+                error.Height = 30;
+                error.Width = 450;
+
+                popup.Controls.Add(error);
+                popup.ShowDialog(); 
+            }
       
         }
+
+        // Als er geklikt wordt, wordt de kliklocatie het nieuwe middelpunt en worden de midX, midY en scale aangepast
         private void mouse_click(object sender, MouseEventArgs mea)
         {
-            midX = (mea.X - width / 2)  * scale;
-            midY = (mea.Y - height / 2)  * scale;
-            scale = scale / 2;
             
+            midX += (mea.X - width / 2)  * scale;
+            midY += (mea.Y - height / 2) * scale;
+            scale = scale / 2;
+
+            Tscale.Text = scale.ToString();
+            TmidX.Text = midX.ToString();
+            TmidY.Text = midY.ToString();
+
             panel2.Invalidate();
         }
 
+        // Het lukt ons niet om de onderstaande delen eruit te halen zonder foutmeldingen, we weten niet precies waarom.
         private void label3_Click(object sender, EventArgs e)
         {
 
@@ -172,8 +288,62 @@ namespace WindowsFormsApp1
         {
 
         }
+
+        // Zodra een item uit de list wordt aangeklikt wordt de schaal van het plaatje aangepast 
+        private void listBox1_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+
+            // Schaal, midx en midy per plaatje instellen
+            // Als je een nieuw plaatje aanklikt, gaat deze terug naar de onderstaande standaard instellingen
+            kleurschema = listBox1.Items[listBox1.SelectedIndex].ToString();
+            if (kleurschema == "Standaard")
+            {
+                scale = 0.01;
+                midX = 0;
+                midY = 0;
+                Tscale.Text = scale.ToString();
+                TmidX.Text = midX.ToString();
+                TmidY.Text = midY.ToString();
+            }
+
+            else if (kleurschema == "Kekke kleur")
+            {
+                midX = -0.21185546875;
+                midY = 0.68431640625;
+                scale = 1.953125E-05;
+
+                Tscale.Text = scale.ToString();
+                TmidX.Text = midX.ToString();
+                TmidY.Text = midY.ToString();
+            }
+
+            else if (kleurschema == "Nederlandse Vlag")
+            {
+                midX = 0.03924560546875;
+                midY = 0.660491943359375;
+                scale = 3.0517578125E-05;
+
+                Tscale.Text = scale.ToString();
+                TmidX.Text = midX.ToString();
+                TmidY.Text = midY.ToString();
+            }
+
+            else if (kleurschema == "Regenboog")
+            { 
+                midX = 0.347546997070313;
+                midY = 0.579248657226563;
+                scale = 6.103515625E-07;
+
+                Tscale.Text = scale.ToString();
+                TmidX.Text = midX.ToString();
+                TmidY.Text = midY.ToString();
+            }
+
+            panel2.Invalidate();
+        }
     }
 
+    // Hier wordt een minimalistische Punt klasse gedefineert, om de coordinaten te representeren
     public class Punt
     {
         public double A;
@@ -182,7 +352,6 @@ namespace WindowsFormsApp1
         {
             A = a;
             B = b;
-
         }
     }
 }
